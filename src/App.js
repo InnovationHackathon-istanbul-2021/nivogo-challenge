9f10765fa39867c53fe4fa962c10b018a6f40c48
Şule Şinikoğlu <ssinikoglu@gmail.com>
2021-11-28 14:54:02 +0300

    add scanBarcode initials

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home/Home";
 import CheckTheShoe from "./pages/checkTheShoe/CheckTheShoe";
+import ScanBarcode from "./pages/scanBarcode/ScanBarcode";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/check-the-shoe" element={<CheckTheShoe />} />
+          <Route path="/scan-barcode" element={<ScanBarcode />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/scanBarcode/ScanBarcode.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./scanBarcode.css";
+// import mockShoePhoto from "./images/mockShoePhoto.jpg"
+
+function ScanBarcode() {
+  let navigate = useNavigate();
+  return (
+    <div className="scanTheBarcode">
+      <header className="scanTheBarcode--header">
+        {/* <div>
+        <img
+          src={mockShoePhoto}
+          alt="Shoe Photo"
+          classNameName="rounded-md productPhoto"
+        />
+      </div> */}
+        <div className="flex flex-col">
+          <input
+            type="text"
+            placeholder="Brand Name"
+            className="input input-accent input-bordered"
+          />
+          <input
+            type="text"
+            placeholder="Shoe Model"
+            className="input input-accent input-bordered"
+          />
+          <input
+            type="text"
+            placeholder="Shoe Type"
+            className="input input-accent input-bordered"
+          />
+        </div>
+        <div className="flex">
+          <div className="flex">
+            <label className="cursor-pointer label">
+              <span className="label-text text-grey-600 text-lg">Left</span>
+              <input
+                type="radio"
+                name="opt"
+                checked="checked"
+                className="radio"
+                value="left"
+              />
+            </label>
+          </div>
+          <div className="flex">
+            <label className="cursor-pointer label">
+              <span className="label-text text-grey-600 text-lg">Right</span>
+              <input
+                type="radio"
+                name="opt"
+                checked="checked"
+                className="radio"
+                value="right"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <button className="btn btn-accent btn-lg" onClick={() => {}}>
+            Cancel
+          </button>
+          <button className="btn btn-accent btn-lg" onClick={() => {}}>
+            Check
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+export default ScanBarcode;
