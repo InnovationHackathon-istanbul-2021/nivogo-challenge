ea09b69bec40277c3aa7e96aa078cfb92b9e6216
Emre Isik <emreisik@yandex.com>
2021-11-29 00:18:00 +0300

    Update AccessDatabase.js

--- src/pages/accessDatabase/AccessDatabase.js
+++ src/pages/accessDatabase/AccessDatabase.js
@@ -1,44 +1,68 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./accessDatabase.css";
-import manual from "./images/manual.png";
-import plus_Icon from "./images/plus_Icon.png";
+import React, { useEffect } from "react";
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, child, get } from "firebase/database";
 
 function AccessDatabase() {
-  let navigate = useNavigate();
-  return (
-    <div className="accessDatabase">
-      <header className="accessDatabase--header">
-        <h1 className="accessDatabase--title mb-16">Access Database</h1>
-        <div className="flex flex-wrap flex-col justify-around box-content">
-          <div>
-            <img
-              src={manual}
-              className="manual mb-12 example w-full"
-              alt="logo"
-            />
-          </div>
+  const firebaseConfig = {
+    apiKey: "AIzaSyDOax3-UK6ZOdWAORWZXoZFugmVw2W4K0E",
+    authDomain: "eros-140d7.firebaseapp.com",
+    databaseURL: "https://eros-140d7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "eros-140d7",
+    storageBucket: "eros-140d7.appspot.com",
+    messagingSenderId: "558377699271",
+    appId: "1:558377699271:web:88d7de6a9a541f50a57be2"
+  };
+  const [state, setState] = React.useState([]);
+  const app = initializeApp(firebaseConfig);
+  var databaseArray = [];
+  const dbRef = ref(getDatabase(app));
+  get(child(dbRef, `/`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      setState(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
 
-          <button
-            className="btn btn-accent btn-lg mb-12"
-            onClick={() => navigate("/manuel-search")}
-          >
-            Manual Search
-          </button>
-        </div>
-        <div className="flex flex-wrap flex-col justify-around box-content">
-          <div>
-            <img src={plus_Icon} className="plus-Icon mb-12" alt="logo" />
-          </div>
-          <button
-            className="btn btn-accent btn-lg"
-            onClick={() => navigate("/add-new-type")}
-          >
-            Add New Type
-          </button>
-        </div>
-      </header>
-    </div>
+  return (
+    <div className="overflow-x-auto w-8/12" id="sonuc">
+    <table className="table w-8/12">
+      <thead>
+        <tr>
+          <th>BARKOD</th> 
+          <th>OPTION KODU</th> 
+          <th>ÜRÜN ÇEŞİT</th>
+          <th>CİNSİYET</th>
+          <th>YAŞ GRUBU</th>
+          <th>RENK</th>
+          <th>MODEL ADI</th>
+          <th>MARKA</th>
+          <th>BEDEN</th>
+          <th>FOTO</th>
+        </tr>
+      </thead> 
+      <tbody>
+        {state.map((item, index) => {
+          return (
+            <tr key={index}>
+              <td>{item.BARKOD}</td>
+              <td>{item["OPTION KODU"]}</td>
+              <td>{item["ÜRÜN ÇEŞİT"]}</td>
+              <td>{item["CİNSİYET"]}</td>
+              <td>{item["YAŞ GRUBU"]}</td>
+              <td>{item.RENK}</td>
+              <td>{item["MODEL ADI"]}</td>
+              <td>{item.MARKA}</td>
+              <td>{item.BEDEN}</td>
+              <td><img src={item["FOTO URL"]} alt="foto" width="100" height="100"/></td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  </div>
   );
 }
 export default AccessDatabase;
